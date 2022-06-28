975c403d2cf775a4bb7846c4e3189958385f6e2c
Anton Österberg <anton.osterberg@protonmail.com>
2022-06-29 01:08:03 +0200

    Update template

--- src/args.rs
+++ src/args.rs
@@ -1,48 +1,62 @@
-use clap::{App, Arg, ArgMatches};
+use clap::Parser;
+use termcolor::ColorChoice;
 
-pub fn args<'a>() -> ArgMatches<'a> {
-    let verbosity = Arg::with_name("verbosity")
-        .takes_value(true)
-        .default_value("1")
-        .validator(|n: String| {
-            let range = 0u8..=5u8;
-            let n: u8 = n.parse::<u8>().unwrap();
-            if range.contains(&n) {
-                Ok(())
-            } else {
-                Err("Invalid value".to_string())
-            }
-        })
-        .short("v")
-        .long("verbosity")
-        .help("Set verbosity level, 0 - 5")
-        .long_help("Set the verbosity level, from 0 (least amount of output) to 5 (most verbose). Note that logging level configured via RUST_LOG overrides this setting.");
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    /// Set verbosity level, 0 - 5
+    ///
+    /// Set the verbosity level, from 0 (least amount of output) to 5 (most verbose). Note that
+    /// logging level configured via RUST_LOG overrides this setting.
+    #[clap(short = 'v', long = "verbosity", default_value = "1")]
+    pub verbosity_level: u8,
 
-    let colors = Arg::with_name("colors")
-        .long("colors")
-        .short("C")
-        .takes_value(true)
-        .default_value("auto")
-        .possible_values(&["auto", "never", "always", "ansi"])
-        .value_name("setting")
-        .help("Enable/disable colors")
-        .long_help("Enable or disable output with colors. By default colors will be used if the terminal seems to support colors.");
+    /// Print debug information
+    ///
+    /// Print debug information about current build for binary, useful for when an issue is
+    /// encountered and reported
+    #[clap(short = 'D', long = "debug")]
+    pub print_dbg: bool,
 
-    let debug = Arg::with_name("debug")
-        .takes_value(false)
-        .short("D")
-        .long("debug")
-        .help("Print debug information")
-        .long_help("Print debug information about current build for binary, useful for when an issue is encountered and reported");
+    /// Enable/disable colors
+    ///
+    /// Enable or disable output with colors. By default colors will be used if the terminal seems
+    /// to support colors.
+    #[clap(short = 'C', long = "colors")]
+    use_colors: Option<Colors>,
+}
 
-    let args: ArgMatches = App::new(crate_name!())
-        .about("DESCRIPTION")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .arg(verbosity)
-        .arg(colors)
-        .arg(debug)
-        .get_matches();
+impl Args {
+    pub fn use_colors(&self) -> ColorChoice {
+        match self.use_colors {
+            None => ColorChoice::Auto,
+            Some(Colors::Always) => ColorChoice::Always,
+            Some(Colors::AlwaysAnsi) => ColorChoice::AlwaysAnsi,
+            Some(Colors::Auto) => ColorChoice::Auto,
+            Some(Colors::Never) => ColorChoice::Never,
+        }
+    }
+}
 
-    args
+#[derive(Debug, Copy, Clone)]
+enum Colors {
+    Always,
+    AlwaysAnsi,
+    Auto,
+    Never,
+}
+
+impl std::str::FromStr for Colors {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.to_lowercase();
+        match s.as_str() {
+            "always" => Ok(Colors::Always),
+            "ansi" => Ok(Colors::AlwaysAnsi),
+            "auto" => Ok(Colors::Auto),
+            "never" => Ok(Colors::Never),
+            _ => Err("Invalid color choice"),
+        }
+    }
 }
--- src/fmt.rs
+++ src/fmt.rs
@@ -1,14 +1,5 @@
 use std::io::Write;
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-
-pub fn color_choice(choice: &str) -> ColorChoice {
-    match choice {
-        "auto" => ColorChoice::Auto,
-        "always" => ColorChoice::Always,
-        "ansi" => ColorChoice::AlwaysAnsi,
-        _ => ColorChoice::Never,
-    }
-}
+use termcolor::{Color, ColorSpec, StandardStream, WriteColor};
 
 pub fn write(stream: &mut StandardStream, content: &str, color: Option<Color>) {
     stream.set_color(ColorSpec::new().set_fg(color)).unwrap();
